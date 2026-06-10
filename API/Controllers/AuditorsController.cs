e5b883a2249fc1cf6a4404b47f0204b7dd4fa8bf
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-10 09:41:03 -0600

    feat(create): metodos para el registro de auditores

---
+++ API/Controllers/AuditorsController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Auditors.Commands.CreateAuditor;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditorsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuditorsController(IMediator mediator) => _mediator = mediator;
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([FromBody] CreateAuditorCommand command)
+        {
+            var response = await _mediator.Send(command);
+            return response.Success ? Ok(response) : BadRequest(new { Error = response.ErrorMessage });
+        }
+    }
+}
