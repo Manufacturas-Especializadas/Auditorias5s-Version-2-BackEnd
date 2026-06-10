470f5df1b53f516b54b9b94dd51d9d0b6b92b9a8
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-10 10:21:05 -0600

    feat(create): metodos para el registro de modulo

---
+++ API/Controllers/ModulesController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Modules.Commands.CreateModule;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModulesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ModulesController(IMediator mediator) => _mediator = mediator;
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([FromBody] CreateModuleCommand command)
+        {
+            var response = await _mediator.Send(command);
+            return response.Success ? Ok(response) : BadRequest(new { Error = response.ErrorMessage });
+        }
+    }
+}
---
+++ Application/Modules/Commands/CreateModule/CreateModuleCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Modules.Commands.CreateModule
+{
+    public record CreateModuleCommand(string Name) : IRequest<ModuleResponse>;
+
+    public record ModuleResponse(int ModuleId, string Name, bool Success, string ErrorMessage = "");
+}
