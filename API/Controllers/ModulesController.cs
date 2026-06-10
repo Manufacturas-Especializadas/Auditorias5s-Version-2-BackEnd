b857cec3468864079fc5ed3b4d89fe41531ed236
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-10 10:56:14 -0600

    feat(delete): metodos para eliminar los modulos

--- API/Controllers/ModulesController.cs
+++ API/Controllers/ModulesController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Audits.Queries.GetActiveModules;
 using Application.Modules.Commands.CreateModule;
+using Application.Modules.Commands.DeleteModule;
 using Application.Modules.Commands.UpdateModule;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -33,6 +34,13 @@ namespace API.Controllers
         {
             var response = await _mediator.Send(command);
             return response.Success ? Ok(response) : BadRequest(new { Error = response.ErrorMessage });
+        }
+
+        [HttpDelete("delete/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _mediator.Send(new DeleteModuleCommand(id));
+            return response.Success ? Ok(new { Message = "Módulo desactivado con éxito." }) : BadRequest(new { Error = response.ErrorMessage });
         }
     }
 }
---
+++ Application/Modules/Commands/DeleteModule/DeleteModuleCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Modules.Commands.DeleteModule
+{
+    public record DeleteModuleCommand(int ModuleId) : IRequest<DeleteModuleResponse>;
+
+    public record DeleteModuleResponse(bool Success, string ErrorMessage = "");
+}
